1033e3cd305ce389a68a717c7f71141156adf3a8
jayasurya jeyakodi <jajeyako@microsoft.com>
2018-05-27 13:06:07 +0530

    add view,template

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ var express = require('express'),
     mongoose= require('mongoose'),
     bodyParser = require('body-parser');
 
-
 var app = express();
 
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-        res.send("hello d")
+        res.render("index");
 })
 
 app.listen("7777", function(err){
